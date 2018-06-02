a87421882de39c555cb6c6a5f33d4d28a4d9947b
Mondo <mondogao@gmail.com>
2018-06-02 14:13:47 +0800

    fix systemd

--- init/init.centos.sh
+++ init/init.centos.sh
@@ -2,6 +2,7 @@
 firewall-cmd --zone=public --add-port=8388/tcp --permanent
 firewall-cmd --zone=public --add-port=8787/tcp --permanent
 firewall-cmd --zone=public --add-port=9006/tcp --permanent
+firewall-cmd --reload
 
 # Add shadowsocks systemd service
 cp ./shadowsocksr.service /usr/lib/systemd/system/
