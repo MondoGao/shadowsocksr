3e6329a62255f984458ae403df3093aa4fa14130
Mondo <mondogao@gmail.com>
2018-06-02 13:48:05 +0800

    add init sh for centos

---
+++ init/bbr.install.sh
@@ -0,0 +1,4 @@
+# Install bbr
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
+./bbr.sh
---
+++ init/init.centos.sh
@@ -0,0 +1,13 @@
+# Config firewall
+firewall-cmd --zone=public --add-port=8388/tcp --permanent
+firewall-cmd --zone=public --add-port=8787/tcp --permanent
+firewall-cmd --zone=public --add-port=9006/tcp --permanent
+
+# Add shadowsocks systemd service
+cp ./shadowsocksr.service /usr/lib/systemd/system/
+systemctl enable shadowsocksr.service
+
+# Config bbr
+cat ./sysctl.conf > /etc/sysctl.conf
+sysctl -p
+systemctl restart shadowsocksr.service
